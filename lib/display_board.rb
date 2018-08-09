b4f92b176fd3a88cf915ba27bca78c27e7f19e89
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-09 22:27:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(arg)
-  puts "   " + "|" + "   " + "|" + "   "
+  puts " arg[0] " + "|" + " arg[1] " + "|" + " arg[2] "
   puts "-----------"
-  puts "   " + "|" + "   " + "|" + "   "
+  puts " arg[3] " + "|" + " arg[4] " + "|" + " arg[5] "
   puts "-----------"
-  puts "   " + "|" + "   " + "|" + "   "
+  puts " arg[6] " + "|" + " arg[7] " + "|" + " arg[8] "
 end
